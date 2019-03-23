326f5fea4cdcb01ef576ec4863d007847847736a
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdm@gmail.com>
2019-03-23 13:39:26 -0400

    api con asure

---
+++ productoBack/productoAPI/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace productoAPI.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<productoAPI.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBack/productoAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace productoAPI.Models
+{
+
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    [Display(Name = "Product Name")]
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+}
+}
